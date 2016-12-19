1ac51d84797397133059c0d93e9126ab92fe493e
zhangmengyuan <1005689344@qq.com>
2016-12-19 19:50:40 +0800

    求球动态网页实现

--- thinkphp_qiuqiu/Application/Home/Controller/IndexController.class.php
+++ thinkphp_qiuqiu/Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,21 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+    //新闻获取
+        $newModel = M('news');
+        $newData = $newModel->select();
+        $this->assign('news',$newData);
+
+
+    //首页信息获取
+        $indexData = M()->query("select x.username,x.signature,x.head,y.class,y.ncontent,y.uploadpic
+                                 from users x,acts y
+                                 where x.pid = y.pid ");
+
+        $this->assign('indexdata',$indexData);
+
+
+
         $this->display();
     }
 
