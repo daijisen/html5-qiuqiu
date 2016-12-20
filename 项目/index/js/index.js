9458bf3439f1edc9e310c8e57210a466bb28023b
Zixu <347937686@qq.com>
2016-12-20 11:03:44 +0800

    首页基本功能实现

--- 项目/index/js/index.js
+++ 项目/index/js/index.js
@@ -1,15 +1,1 @@
-function ChangeDiv(divId,divName,zDivCount)
 
-{ 
-
-for(i=0;i<zDivCount;i++)
-
-{
-
-document.getElementById(divName+i).style.display="none"; 
-
-}
-
-document.getElementById(divName+divId).style.display="";
-
-}
