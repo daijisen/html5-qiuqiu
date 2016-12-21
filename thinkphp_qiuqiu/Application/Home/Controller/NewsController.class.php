fafd35bfd3da818f6618093d5f015267cef5fe32
zhangmengyuan <1005689344@qq.com>
2016-12-21 19:34:35 +0800

    求球动态网页实现

--- thinkphp_qiuqiu/Application/Home/Controller/NewsController.class.php
+++ thinkphp_qiuqiu/Application/Home/Controller/NewsController.class.php
@@ -3,40 +3,15 @@ namespace Home\Controller;
 use Think\Controller;
 class NewsController extends Controller {
     public function index(){
-    //新闻获取
+    //新闻内容获取
 
-        //NBA
-        $new1Model = M('news');
-        $new1Data = $new1Model->where("type=1")->select();
-        $this->assign('newsNBA',$new1Data);
-        //CBA
-        $new2Model = M('news');
-        $new2Data = $new2Model->where("type=2")->select();
-        $this->assign('newsCBA',$new2Data);
-        //中超
-        $new3Model = M('news');
-        $new3Data = $new3Model->where("type=3")->select();
-        $this->assign('newsZhong',$new3Data);
-        //西甲
-        $new4Model = M('news');
-        $new4Data = $new4Model->where("type=4")->select();
-        $this->assign('newsXi',$new4Data);
-        //英超
-        $new5Model = M('news');
-        $new5Data = $new5Model->where("type=5")->select();
-        $this->assign('newsYing',$new5Data);
-
-
-    //首页信息获取
-        $indexData = M()->query("select x.username,x.signature,x.head,y.class,y.ncontent,y.uploadpic
-                                 from users x,acts y
-                                 where x.pid = y.pid ");
-
-        $this->assign('indexdata',$indexData);
-
-
+        $nid = I('nid');
+        $sql = "select news.*,name from news,newstype where news.type = newstype.type and nid=$nid order by nid desc";
+        $new1Data = M()->query($sql);
+        $this->assign('newsContent',$new1Data[0]);
 
         $this->display();
     }
+
 
 }
