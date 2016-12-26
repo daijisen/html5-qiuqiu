231360338a119f373206f4a1c05be64449f14ff6
zhangmengyuan <1005689344@qq.com>
2016-12-26 10:45:21 +0800

    求球动态网页实现

--- thinkphp_qiuqiu/Application/Home/Controller/CommentController.class.php
+++ thinkphp_qiuqiu/Application/Home/Controller/CommentController.class.php
@@ -4,23 +4,21 @@ use Think\Controller;
 class CommentController extends Controller {
     public function index(){
        $aid = I('aid');
-       $data['id'] = $aid;
-       dump($data);
+       $data['id'] =  intval($aid);
        $this->assign('aid',$data);
        $this->display();
     }
     public function store(){
             //生成模型对象
             if(isset($_SESSION['uid'])){
-                $aid = I('aid');
                 $pid = $_SESSION['uid'];
-                dump($aid);
+                $aid = $_GET['aid'];
                 $repModel = M('reply');
                 // 根据表单提交的POST数据创建数据对象
                 $data = $repModel->create();
 
                  $time = date("Y-m-d H:i:s");
-                 $data['sendtime'] = $time;
+                 $data['time'] = $time;
                  $data['aid'] = $aid;
                  $data['pid'] = $pid;
 
